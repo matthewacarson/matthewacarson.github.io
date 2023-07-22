99a07d24c364c8fdfec97979e5ea3e917f31ef71
matthewacarson <github-carsonm@snkmail.com>
2023-07-22 14:07:45 -0700

    upload pdfs and delete default posts

--- git_commands.R
+++ git_commands.R
@@ -1,3 +1,14 @@
 system("git init")
 
 system("git remote add origin https://github.com/matthewacarson/matthewacarson.github.io.git")
+
+# Use the following in PowerShell or Command Prompt
+# 
+# To publsih pages to the web: 
+#   quarto publish gh-pages
+# 
+# To update source code:
+# 1) Naviage to the folder I want to commit (use cd command).
+#   git add .
+#   git commit -m "Your commit message here"
+#   git push origin main
