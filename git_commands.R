a9f34a27921653a4a1a2e4dbbdc4757790f47cf3
matthewacarson <github-carsonm@snkmail.com>
2023-07-22 15:58:28 -0700

    1st blog post with imgs

--- git_commands.R
+++ git_commands.R
@@ -5,10 +5,13 @@ system("git remote add origin https://github.com/matthewacarson/matthewacarson.g
 # Use the following in PowerShell or Command Prompt
 # 
 # To publsih pages to the web: 
-#   quarto publish gh-pages
+# 1) Render qmd files in R Studio
+# 2) quarto publish gh-pages
 # 
 # To update source code:
-# 1) Naviage to the folder I want to commit (use cd command).
+# 1) Navigate to the folder I want to commit (use cd command).
 #   git add .
 #   git commit -m "Your commit message here"
 #   git push origin main
+#   
+#   (This can also be accomplished through R Studio using Git)
